1834bad2545f3070a6f3f112ae8515317be1ba88
Yatin005 <68661297+Yatin005@users.noreply.github.com>
2025-02-21 14:36:43 -0500

    done

---
+++ week_6(Protocol)/week_6(Protocol)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week_6(Protocol)
+//
+//  Created by Yatin Parulkar on 2025-02-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
